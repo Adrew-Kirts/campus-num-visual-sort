8ceda38d7679255f4a7419ec06904e620e36c25d
Ezra Strikwerda <ezra@strikwerda.fr>
2023-08-02 14:39:32 +0200

    Quicksort done

--- src/main/java/edu/campusnum/visualsort/sort/MergeSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/MergeSort.java
@@ -2,43 +2,48 @@ package edu.campusnum.visualsort.sort;
 
 import edu.campusnum.visualsort.model.ObservableArray;
 
-
-/**
- * Created by Web 74 all right reserved
- * User: vincent
- * Date: 27/07/2023
- * Time: 16:31
- */
 public class MergeSort implements SortAlgorithm {
+
     @Override
     public void sort(ObservableArray array) {
-//        mergeSort(array);
+        int[] auxArray = new int[array.getLength()];
+        mergesort(array, auxArray, 0, array.getLength() - 1);
     }
 
-//    public ObservableArray mergeSort(ObservableArray array) {
-//        int n = array.getLength();
-//        int mid = n / 2;
-//        if (n < 1) {
-//            return array;
-//        } else {
-//            ObservableArray left = array.slice(0, mid);
-//            ObservableArray right = array.slice(mid + 1, n);
-//            return merge(mergeSort(left), mergeSort(right));
-//        }
-//    }
+    private void mergesort(ObservableArray array, int[] auxArray, int first, int last) {
+        if (first < last) {
+            int mid = (first + last) / 2;
+            mergesort(array, auxArray, first, mid);
+            mergesort(array, auxArray, mid + 1, last);
+            merge(array, auxArray, first, mid, last);
+        }
+    }
 
-//    public ObservableArray merge(ObservableArray left, ObservableArray right) {
-//        if (left.getLength() == 0) {
-//            return right;
-//        }
-//        if (right.getLength() == 0) {
-//            return left;
-//        }
-////        if (left.get(1) < right.get(1)) {
-////            return; //A[1] ⊕ fusion(A[2, ..., a], B)
-////        } else {
-////            return; //B[1] ⊕ fusion(A, B[2, ..., b])
-////        }
-////    }
+    private void merge(ObservableArray array, int[] auxArray, int first, int mid, int last) {
+        // Copy elements to the auxiliary array
+        for (int i = first; i <= last; i++) {
+            auxArray[i] = array.get(i);
+        }
+
+        int i = first;
+        int j = mid + 1;
+        int k = first;
+
+        while (i <= mid && j <= last) {
+            if (auxArray[i] <= auxArray[j]) {
+                array.swap(k, i);
+                i++;
+            } else {
+                array.swap(k, j);
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            array.swap(k, i);
+            i++;
+            k++;
+        }
+    }
 }
-
--- src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
@@ -8,18 +8,35 @@ import edu.campusnum.visualsort.model.ObservableArray;
  * Date: 27/07/2023
  * Time: 16:31
  */
-public class QuickSort implements SortAlgorithm{
+public class QuickSort implements SortAlgorithm {
 
     @Override
     public void sort(ObservableArray array) {
-        int first = array.get(0);
-        int last = array.getLength(); //maybe -1??
+        int first = 0;
+        int last = array.getLength()-1;
         quicksort(array, first, last);
     }
 
-    public void quicksort(ObservableArray array, int first, int last){
-
+    public void quicksort(ObservableArray array, int first, int last) {
+        if (last - first < 1) {
+            return;
+        }
+        int pi = partition(array, first, last);
+        quicksort(array, first, pi - 1);
+        quicksort(array, pi + 1, last);
     }
 
+    public int partition(ObservableArray array, int first, int last) {
+        int pivot = array.get(last);
+        int j = first - 1;
+        for (int i = first; i <= last -1; i++) {
+            if (array.get(i) <= pivot) {
+                j++;
+                array.swap(i, j);
+            }
+        }
+        array.swap(last, j+1);
+        return j+1;
+    }
 
 }
